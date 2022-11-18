a1cb27e14deab7e2134f9b0ae0f0b8dd83f3cdc5
D. Prameswara <meshwara@gmail.com>
2022-11-18 09:52:52 +0700

    Initial Commit

---
+++ ColorFromText/ColorFromTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorFromTextApp.swift
+//  ColorFromText
+//
+//  Created by D. Prameswara on 18/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ColorFromTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
